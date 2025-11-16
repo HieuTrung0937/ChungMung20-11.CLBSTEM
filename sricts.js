1ee6412697ec45d4af7307b03ad409528a257b5a
HieuTrung0937 <ngotsacombank@gmail.com>
2025-11-16 22:33:39 +0700

    capnhat animaiton 2

--- sricts.js
+++ sricts.js
@@ -4,3 +4,31 @@ window.addEventListener("load", () => {
     card.classList.add("show");
   }, 500); // chờ load xong
 });
+const photos = document.querySelectorAll(".photo");
+
+function checkPhotos() {
+  photos.forEach((img) => {
+    const rect = img.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 80) {
+      img.classList.add("show");
+    }
+  });
+}
+
+window.addEventListener("scroll", checkPhotos);
+checkPhotos();
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+
+photos.forEach((photo) => {
+  const openLightbox = () => {
+    lightboxImg.src = photo.src;
+    lightbox.style.display = "flex";
+  };
+  photo.addEventListener("click", openLightbox);
+  photo.addEventListener("touchstart", openLightbox); // hỗ trợ điện thoại
+});
+
+lightbox.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
